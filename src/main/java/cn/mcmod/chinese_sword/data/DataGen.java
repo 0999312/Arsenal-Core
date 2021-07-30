968f68bcd87a751479e5850dadf77b43101e8bab
unknown <617510630@qq.com>
2021-07-31 00:46:42 +0800

    RRRRRRRRRRRRRRECIPES

--- src/main/java/cn/mcmod/chinese_sword/data/DataGen.java
+++ src/main/java/cn/mcmod/chinese_sword/data/DataGen.java
@@ -8,11 +8,8 @@ import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 public class DataGen {
     @SubscribeEvent
     public static void dataGen(GatherDataEvent event) {
-        // 鞘的合成表Data Generator, 类型为合成台的有序合成。
-        event.getGenerator().addProvider(new SheathRecipes(event.getGenerator()));
-        // 锻造台合成表Data Generator, 类型为锻造台的合成，合成只有左右两格。
-        // 比铁砧更加优雅！
-        event.getGenerator().addProvider(new SmithRecipes(event.getGenerator()));
+        // 合成表Data Generator, 类型为合成台的有序合成。
+        event.getGenerator().addProvider(new SwordRecipes(event.getGenerator()));
         // 生成中式剑模型的Data Generator, 提供了 public 的两种剑的Model Builder.
         // 物品模型大量生成的典型案例。
         event.getGenerator().addProvider(new SwordModelProvider(event.getGenerator(), event.getExistingFileHelper()));
